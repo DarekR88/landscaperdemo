7b687ff9f100ffa80aeb11fd46b56adee1787cad
Darek Radke <dradmclad@gmail.com>
2024-03-26 15:04:45 -0500

    css for navbar and home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import closePinkImg from "../../public/images/blossoms.jpg";
 export default function Home() {
   return (
     <main className="">
-      <div className="flex lg:flex-row ">
-        <div className="lg:w-1/4 lg:overflow-hidden lg:relative">
+      <div className="flex lg:flex-row">
+        <div className="lg:w-1/4 lg:h-[465px] lg:overflow-hidden lg:relative">
           <Image src={bigPinkImg} alt="yard" />
         </div>
-        <div className="lg:w-1/2">
-          <p>
+        <div className="lg:w-1/2 m-auto">
+          <h1 className="text-center text-5xl lg:mb-[15px]">Go Green with GROWGREEN</h1>
+          <p className="px-3 text-xl">
             At GrowGreen Lawn and Landscaping, we're more than just a lawn care
             and landscaping company — we're your partners in creating and
             maintaining the outdoor space of your dreams. With a deep-rooted
@@ -20,11 +21,19 @@ export default function Home() {
             clients not just satisfied, but delighted. Welcome to GrowGreen,
             where your lawn's potential meets our passion for perfection.
           </p>
+          <h1 className="text-center text-4xl lg:mt-[30px] lg:mb-[15px]">"My yard has never looked better!"</h1>
+          <p className="text-center text-xl">-Mark Rerwin</p>
+          <p className="text-center">Satisfied Customer</p>
         </div>
-        <div className="lg:w-1/4 lg:overflow-hidden lg:relative">
+        <div className="lg:w-1/4 lg:h-[465px] lg:overflow-hidden lg:relative">
           <Image src={closePinkImg} alt="yard" />
         </div>
       </div>
+
+      <div className="w-full h-20px bg-[#283618]">
+        
+      </div>
+
     </main>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -30,11 +30,7 @@ export function NavBar() {
   }, []); // Run only on component mount
 
   return (
-    <main
-      className={`transition-all duration-700 ${
-        isVisible ? "opacity-100" : "opacity-0"
-      }`}
-    >
+    <main>
       <div
         className={`fixed flex flex-col w-screen h-screen z-10 bg-white text-center pt-20 gap-8 ${
           isOpen
@@ -67,7 +63,7 @@ export function NavBar() {
           About
         </Link>
       </div>
-      <header className="flex flex-wrap gap-10 py-4 items-center w-screen lg:flex-row lg:justify-between lg:px-20 lg:h-28 lg:mb-[75px] bg-[#283618] text-white">
+      <header className="flex flex-wrap gap-10 py-4 items-center w-screen lg:flex-row lg:justify-between lg:px-20 lg:h-28 lg:mb-[0px] bg-[#283618] text-white">
         <Link
           href={"/"}
           className="font-Playfair flex flex-col lg:flex-row lg:items-center lg:justify-center lg:p-2 tracking-widest text-xl z-0 bg-white"
@@ -108,7 +104,7 @@ export function NavBar() {
           >
             Home
           </Link>
-          <Link
+          {/* <Link
             className="text-l relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:focus:scale-x-100 after:transition after:duration-300 after:origin-center"
             href="/websites"
           >
@@ -119,7 +115,7 @@ export function NavBar() {
             href="/social"
           >
             Social Media
-          </Link>
+          </Link> */}
           {/* <Link
             className="text-l relative w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-black after:w-full after:scale-x-0 after:hover:scale-x-100 after:focus:scale-x-100 after:transition after:duration-300 after:origin-center"
             href="/advertising"
